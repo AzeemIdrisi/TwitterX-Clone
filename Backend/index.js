7b4532521378d43b3c04fb8a7fd4747c2bdf7ca0
Azeem Idrisi <azeemidrisi@protonmail.com>
2024-04-28 15:30:34 +0530

    Updated backend and frontend URLs

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "zenix-twitter.vercel.app"],
   credentials: true,
 };
 
--- Frontend/src/utils/constant.js
+++ Frontend/src/utils/constant.js
@@ -1,2 +1,4 @@
-export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
-export const POST_API_END_POINT = "http://localhost:8000/api/v1/post";
+// export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
+// export const POST_API_END_POINT = "http://localhost:8000/api/v1/post";
+export const USER_API_END_POINT = "https://zenix-rouge.vercel.app/api/v1/user";
+export const POST_API_END_POINT = "https://zenix-rouge.vercel.app/api/v1/post";
